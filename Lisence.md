075ed578d6bd0189f3d6e1e8613b66021c08bbf7
SakritUser123 <veerendrasakthi.prabhurajan@gmail.com>
2024-02-08 21:45:59 -0500

    removed unwanted modules

---
+++ Lisence.md
@@ -0,0 +1,1 @@
+This is a ball game that I own!!!!
---
+++ MySql.session.sql
@@ -0,0 +1,16 @@
+CREATE TABLE user_info (
+      username varchar(20),
+      email varchar(50),
+      age integer(5)
+      
+);
+
+
+
+select * from user_info
+
+insert into user_info (username,email,age) values('Sakthi', 'sakthi.test@gmail.com', 14)
+
+
+
+
